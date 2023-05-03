9b40ef4a49817707f3bb2453c7e45477fb870c6e
Mykyta Nedialkov <mykyta.nedialkov@webpros.com>
2023-05-03 20:23:05 +0200

    Add SelectComponent

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <SelectComponent />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import SelectComponent from "./components/SelectComponent.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    SelectComponent,
   },
 };
 </script>
---
+++ src/components/SelectComponent.vue
@@ -0,0 +1,13 @@
+<template>
+  <select name="test" id="test"></select>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
